a2b534be7b5075eb0584f73b91567e38186d29fc
Rivaldi <63333919+rvldpw@users.noreply.github.com>
2026-04-26 21:26:24 +0700

    Refactor import statements in recommender.py
    Updated import statements to remove 'src' prefix.

--- src/recommender.py
+++ src/recommender.py
@@ -3,14 +3,14 @@ import numpy as np
 
 from sklearn.metrics.pairwise import cosine_similarity
 
-from src.feature_engineering import (
+from feature_engineering import (
     add_recency_weight,
     build_movie_feature_matrix,
     build_user_profile,
     GENRE_COLS
 )
 
-from src.collaborative import CollaborativeEngine
+from collaborative import CollaborativeEngine
 
 class RecommenderSystem:
 
